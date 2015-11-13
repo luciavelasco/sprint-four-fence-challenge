e7a7ff13c5c07b299bd1c645b1a614102a920e5c
Lucia Velasco <lucia.velasco@mayden.co.uk>
2015-11-13 13:56:00 +0000

    decimals

--- fence-challenge-frontend.php
+++ fence-challenge-frontend.php
@@ -9,7 +9,7 @@ require 'fence-challenge.php';
             <label>Enter Number of Poles You Have: <input type="number" name="posts" placeholder="Enter Text..." ></label>
             <label>Enter Number of Rails You Have: <input type="number" name="rails" placeholder="Enter Text..."></label>
             <p> OR: </p>
-            <label>Enter Number Distance of Fence You Wish to Build: <input type="text" name="length" placeholder="Enter Text..."></label>
+            <label>Enter Number Distance of Fence You Wish to Build: <input type="number" step="any" name="length" placeholder="Enter Text..."></label>
             <input type="submit" value="Submit">
         </form>
         <p><?php echo $returnLength; ?></p>
